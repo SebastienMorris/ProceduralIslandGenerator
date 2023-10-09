9dc0b51906ec25a346b1eaa5306ba35be1cf727e
SebastienMorris <sebw.morris@gmail.com>
2023-10-09 16:12:11 +0200

    finished noise generator

--- Assets/Scripts/IslandNoise.cs
+++ Assets/Scripts/IslandNoise.cs
@@ -3,70 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 using System;
+using UnityEngine.UI;
 
 public class IslandNoise : MonoBehaviour
 {
-    public int TextureSize;
-    public float NoiseScale, IslandSize;
-    [Range(1, 20)] public int NoiseOctaves;
-    [Range(0, 9999999)] int Seed;
+    [SerializeField] private int resolution = 100;
+    [SerializeField] private float noiseScale = 0.25f, islandSize = 40;
+
+    [Range(1, 20)] [SerializeField] private int nbOctaves = 1;
+    [Range(0, 9999999)] [SerializeField] private int seed;
 
     private Color[] colours;
     private Texture2D texture;
 
-    public Gradient colourGradient;
+    [SerializeField] private Gradient colourGradient;
 
-    private void Start()
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private bool applyFallOffMap = true;
+
+    [Min(1f)] [SerializeField] private float persistance = 2f;
+
+    private void OnValidate()
     {
-        texture = new Texture2D(TextureSize, TextureSize);
+        GenerateNoiseMap();
+    }
+
+    private void GenerateNoiseMap()
+    {
+        print("Starting");
+
+        texture = new Texture2D(resolution, resolution);
         colours = new Color[texture.height * texture.width];
 
-        Renderer renderer = GetComponent<MeshRenderer>();
-        renderer.sharedMaterial.mainTexture = texture;
+        GetComponent<RawImage>().texture = texture;
 
-        Vector2 Org = new Vector2(Mathf.Sqrt(Seed), Mathf.Sqrt(Seed));
+        Vector2 origin = new Vector2(Mathf.Sqrt(seed), Mathf.Sqrt(seed));
 
-        for(int x=0, i=0; x<TextureSize; x++, i++)
+        for (int x = 0, i = 0; x < resolution; x++)
         {
-            for(int y=0; y<TextureSize; y++, i++)
+            for (int y = 0; y < resolution; y++, i++)
             {
-                float a = CreateNoise(x, y, Org);
-                colours[i] = colourGradient.Evaluate(a);
+                colours[i] = colourGradient.Evaluate(CreateNoise(x, y, origin));
             }
         }
         texture.SetPixels(colours);
         texture.Apply();
         texture.wrapMode = TextureWrapMode.Clamp;
+        print("Ending");
     }
 
-    private float CreateNoise(int x, int y, Vector2 Origin)
+    private float CreateNoise(float x, float y, Vector2 origin)
     {
         float noiseValue = 0;
 
-        float frequency = NoiseScale;
-        float amplitude = 1f;
+        float amplitude = noiseScale;
+        float frequency = 1f;
 
-        for(int i=0; i < NoiseOctaves; i++)
+        for(int i=0; i < nbOctaves; i++)
         {
-            float xVal = (x / (frequency * TextureSize)) + Origin.x;
-            float yVal = (y / (frequency * TextureSize)) + Origin.y;
+            float xVal = (x / (amplitude * resolution)) + origin.x + offset.x;
+            float yVal = (y / (amplitude * resolution)) + origin.y + offset.y;
 
             float res = noise.snoise(new float2(xVal, yVal));
 
-            noiseValue += Mathf.InverseLerp(0, 1, res) / amplitude;
+            noiseValue += Mathf.InverseLerp(0, 1, res) / frequency;
 
-            frequency /= 2f;
-            amplitude *= 2f;
+            amplitude /= persistance;
+            frequency *= persistance;
         }
-
-        return noiseValue -= FallOffMap(x, y, TextureSize, IslandSize);
+        if (applyFallOffMap)
+            return noiseValue -= FallOffMap(x, y, resolution, islandSize);
+        else
+            return noiseValue;
     }
 
     private float FallOffMap(float x, float y, float size, float IslandSize)
     {
         float gradient = 1f;
 
+        gradient /= (x * y) / (size * size) * (1 - (x / size)) * (1 - (y / size));
+        gradient -= 16;
+        gradient /= IslandSize;
 
-        return 0.1f;
+        return gradient;
     }
 }
