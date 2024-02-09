b58ea9944786ed7a741bb0cf38cb62e45aac639c
SebastienMorris <sebw.morris@gmail.com>
2024-02-09 15:57:46 +0100

    add hash function in compute shader v1

--- Assets/Scripts/Noise Improvements/HashVisualization.cs
+++ Assets/Scripts/Noise Improvements/HashVisualization.cs
@@ -20,48 +20,48 @@ public class HashVisualization : MonoBehaviour
         }
     }
 
-    static int hashesId = Shader.PropertyToID("_Hashes");
-    static int configId = Shader.PropertyToID("_Config");
+    static int _hashesId = Shader.PropertyToID("_Hashes");
+    static int _configId = Shader.PropertyToID("_Config");
 
     [SerializeField] private Mesh instanceMesh;
     [SerializeField] private Material material;
 
     [SerializeField, Range(1, 512)] private int resolution = 16;
 
-    private NativeArray<uint> hashes;
+    private NativeArray<uint> _hashes;
 
-    private ComputeBuffer hashesBuffer;
+    private ComputeBuffer _hashesBuffer;
 
-    private MaterialPropertyBlock propertyBlock;
+    private MaterialPropertyBlock _propertyBlock;
 
     private void OnEnable()
     {
         int length = resolution * resolution;
-        hashes = new NativeArray<uint>(length, Allocator.Persistent);
-        hashesBuffer = new ComputeBuffer(length, 4);
+        _hashes = new NativeArray<uint>(length, Allocator.Persistent);
+        _hashesBuffer = new ComputeBuffer(length, 4);
 
         new HashJob
         {
-            hashes = hashes
-        }.ScheduleParallel(hashes.Length, resolution, default).Complete();
+            hashes = _hashes
+        }.ScheduleParallel(_hashes.Length, resolution, default).Complete();
 
-        hashesBuffer.SetData(hashes);
+        _hashesBuffer.SetData(_hashes);
 
-        propertyBlock ??= new MaterialPropertyBlock();
-        propertyBlock.SetBuffer(hashesId, hashesBuffer);
-        propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution));
+        _propertyBlock ??= new MaterialPropertyBlock();
+        _propertyBlock.SetBuffer(_hashesId, _hashesBuffer);
+        _propertyBlock.SetVector(_configId, new Vector4(resolution, 1f / resolution));
     }
 
     private void OnDisable()
     {
-        hashes.Dispose();
-        hashesBuffer.Release();
-        hashesBuffer = null;
+        _hashes.Dispose();
+        _hashesBuffer.Release();
+        _hashesBuffer = null;
     }
 
     private void OnValidate()
     {
-        if(hashesBuffer != null && enabled)
+        if(_hashesBuffer != null && enabled)
         {
             OnDisable();
             OnEnable();
@@ -70,6 +70,6 @@ public class HashVisualization : MonoBehaviour
 
     private void Update()
     {
-        Graphics.DrawMeshInstancedProcedural(instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one), hashes.Length, propertyBlock);
+        Graphics.DrawMeshInstancedProcedural(instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one), _hashes.Length, _propertyBlock);
     }
 }
