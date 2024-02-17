34e7adef123be4f82817b72c3729acf8d1dcab73
SebastienMorris <85884910+SebastienMorris@users.noreply.github.com>
2024-02-17 16:23:20 +0100

    working compute shader cube grid spawn with gradient material

--- Assets/Noise Improvements/HashInclude.hlsl
+++ Assets/Noise Improvements/HashInclude.hlsl
@@ -4,6 +4,20 @@ StructuredBuffer<uint> _Hashes;
 
 float4 _Config;
 
+[numthreads(8, 8, 1)]
+void ConfigureProcedural()
+{
+    #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+    float v = floor(_Config.y * unity_InstanceID);
+    float u = unity_InstanceID -  _Config.x * v;
+
+    unity_ObjectToWorld = 0.0;
+    unity_ObjectToWorld._m03_m13_m23_m33 = float4(_Config.y * (u + 0.5) - 0.5, 0.0, _Config.y * (v + 0.5) - 0.5, 1.0);
+    unity_ObjectToWorld._m00_m11_m22 = _Config.y;
+		
+    #endif
+}
+
 float3 GetHashColour()
 {
     #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
