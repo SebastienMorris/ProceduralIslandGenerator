9ebb3fc215a38276dd360ddb1a6124fc57d693e6
CamillePele <>
2024-03-01 09:19:58 +0100

    implemented noise with marching cubes c#

--- Assets/Scripts/ChunkMarchingCube.cs
+++ Assets/Scripts/ChunkMarchingCube.cs
@@ -1,21 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
+
+using static Unity.Mathematics.math;
+using static Noise;
+using Vector3 = UnityEngine.Vector3;
 
 public class ChunkMarchingCube : MonoBehaviour
 {
     [SerializeField] private Vector3Int dimensions = new Vector3Int(0, 0, 0);
     [SerializeField] [Range(0, 1)] private float surfaceLevel = 0f;
 
-    [Min(0.01f)][SerializeField] private float noiseScale = 0.01f;
-    [Range(1, 20)] [SerializeField] private int octaves;
-    [Range(0, 1)] [SerializeField] private float persistance;
-    [Min(1)] [SerializeField] private float lacunarity;
-    [SerializeField] private int seed;
-    [SerializeField] private Vector3 noiseOffset = new Vector3(0f, 0f, 0f);
+    [SerializeField] private Noise.Settings noiseSettings;
+    [SerializeField] private SpaceTRS domainTRS;
 
-
-    [SerializeField] [Min(1)] private int cubeSize = 1;
+    
     [SerializeField] private GameObject largeMarchingCubePrefab;
     [SerializeField] private int chunkSize = 10;
 
@@ -33,6 +39,13 @@ public class ChunkMarchingCube : MonoBehaviour
     [SerializeField] private bool debug;
 
     private List<GameObject> listChunks = new List<GameObject>();
+
+    private NativeArray<float3x4> positions;
+    private NativeArray<float4> noise4;
+    
+    private float[] finalNoise;
+
+    private float[] chunkNoise;
 
     private void Update()
     {
@@ -56,25 +69,34 @@ public class ChunkMarchingCube : MonoBehaviour
         }
     }
 
-    private void CreateSeedValue()
-    {
-        //Random.InitState(stringSeed.GetHashCode());
-    }
-
     private void CreateChunks()
     {
-        float[,,] fallOffMapValues = new float[dimensions.x + 1, dimensions.y + 1, dimensions.z + 1];
+        int length = dimensions.x * dimensions.y * dimensions.z;
+        length = length / 4 + (length & 1);
+
+        positions = new NativeArray<float3x4>(length, Allocator.Persistent);
+        noise4 = new NativeArray<float4>(length, Allocator.Persistent);
+        //finalNoise = new NativeArray<float>(length, Allocator.Persistent);
+        
+        GetPositions(length);
+        CreateNoise(length);
+        
         if (useFallOffMap)
         {
-            fallOffMapValues = fallOffMap.GenerateFallOffMap(new Vector3Int(dimensions.x + 1, dimensions.y + 1, dimensions.z + 1), steepness, centerSize);
+            float[,,] fallOffMapValues = fallOffMap.GenerateCircularFallOffMap(new Vector3Int(dimensions.x, dimensions.y, dimensions.z), steepness, centerSize);
+            ApplyFalloffToNoise(noise4.Reinterpret<float>(4 * 4), fallOffMapValues);
             //fallOffMapValues = fallOffMap.GenerateFallOffMap(new Vector3Int(dimensions.x + 1, dimensions.y + 1, dimensions.z + 1), fallOffCurve);
+        }
+        else
+        {
+            finalNoise = noise4.Reinterpret<float>(4 * 4).ToArray();
         }
 
         int nbChunksX = dimensions.x / chunkSize;
         int nbChunksY = dimensions.y / chunkSize;
         int nbChunksZ = dimensions.z / chunkSize;
 
-        int nbChunks = nbChunksX * nbChunksY * nbChunksZ;
+        int nbChunk = 0;
 
         for (int x=0; x < nbChunksX; x++)
         {
@@ -82,16 +104,106 @@ public class ChunkMarchingCube : MonoBehaviour
             {
                 for(int z=0; z < nbChunksZ; z++)
                 {
-                    Vector3 chunkPos = transform.position + new Vector3((x - nbChunksX / 2) + 0.5f, (y - nbChunksY / 2) + 0.5f, (z - nbChunksZ / 2) + 0.5f) * chunkSize;
+                    Vector3 chunkPos = transform.position + new Vector3(x - nbChunksX / 2 + 0.5f, y - nbChunksY / 2 + 0.5f, z - nbChunksZ / 2 + 0.5f) * chunkSize;
                     GameObject spawnedChunk = Instantiate(largeMarchingCubePrefab, chunkPos, transform.rotation, transform);
                     listChunks.Add(spawnedChunk);
-
-                    NoiseData noiseData = LandMassNoise.CreateNoiseData(seed, octaves, noiseScale, persistance, lacunarity);
                     
-                    spawnedChunk.GetComponent<LargeMarchingCube>().StartGeneration( fallOffMapValues, noiseGen, noiseData, interpolate, dimensions, new Vector3Int(chunkSize, chunkSize, chunkSize), surfaceLevel, noiseOffset);
+                    chunkNoise = new float[chunkSize * chunkSize * chunkSize];
+                    
+                    GetNoisePortion(nbChunk, chunkSize);
+                    
+                    spawnedChunk.GetComponent<LargeMarchingCube>().StartGeneration(chunkNoise, interpolate, new Vector3Int(chunkSize, chunkSize, chunkSize), surfaceLevel);
+                    nbChunk++;
                 }
             }
         }
+        positions.Dispose();
+        noise4.Dispose();
+    }
+
+    private void GetNoisePortion(int nbChunk, int size)
+    {
+        int iterations = size * size * size;
+        for (int i = 0; i < chunkNoise.Length; i++)
+        {
+            chunkNoise[i] =  finalNoise[(nbChunk * iterations) + i];
+        }
+    }
+
+    private void ApplyFalloffToNoise(NativeArray<float> noise, float[,,] falloff)
+    {
+        finalNoise = new float[noise.Length];
+        print(falloff.Length);
+        int i = 0;
+        foreach (float f in falloff)
+        {
+            finalNoise[i] = noise[i] * abs(f - 1f);
+            i++;
+        }
+    }
+
+    private void GetPositions(int length)
+    {
+        float3[] pos = new float3[length * 4];
+        
+        int i = 0;
+        for (int x = 0; x < dimensions.x; x++)
+        {
+            for (int y = 0; y < dimensions.y ; y++)
+            {
+                for (int z = 0; z < dimensions.z; z++)
+                {
+                    pos[i] = new float3(x, y, z);
+                    i++;
+                }
+            }
+        }
+        VectorizePos(pos, length, dimensions);
+    }
+
+    private void VectorizePos(float3[] pos, int totalLength, Vector3 dimensions)
+    {
+        int index = 0;
+        float4x4 trs = transform.worldToLocalMatrix;
+        for (int i = 0; i < pos.Length; i += 4)
+        {
+            float4 x = new float4(pos[i].x / dimensions.x, pos[i + 1].x  / dimensions.x, pos[i + 2].x  / dimensions.x, pos[i + 3].x  / dimensions.x);
+            float4 y = new float4(pos[i].y  / dimensions.y, pos[i + 1].y / dimensions.y, pos[i + 2].y / dimensions.y, pos[i + 3].y / dimensions.y);
+            float4 z = new float4(pos[i].z / dimensions.z, pos[i + 1].z / dimensions.z, pos[i + 2].z / dimensions.z, pos[i + 3].z / dimensions.z);
+            
+            positions[index] = transpose(trs.Get3x4().TransformVectors( new float4x3(x - 0.5f, y - 0.5f, z - 0.5f)));
+            index++;
+        }
+    }
+
+    private void CreateNoise(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            float4 res = GenerateNoise(positions[i]);
+            //print(res);
+            noise4[i] = res;
+        }
+    }
+
+    private float4 GenerateNoise(float3x4 positions)
+    {
+        float4x3 position = domainTRS.Matrix.TransformVectors(transpose(positions));
+        var hash = SmallXXHash4.Seed(noiseSettings.seed);
+        int frequency = noiseSettings.frequency;
+        float amplitude = 1f;
+        float amplitudeSum = 0f;
+        float4 sum = 0f;
+        
+        for (int j = 0; j < noiseSettings.octaves; j++)
+        {
+            sum += default(Lattice3D<Perlin, LatticeNormal>).GetNoise4(position, hash + j, frequency) * amplitude;
+            amplitudeSum += amplitude;
+            frequency *= noiseSettings.lacunarity;
+            amplitude *= noiseSettings.persistence;
+        }
+
+        return (sum / amplitudeSum) / 2 + 0.5f;
     }
 
     private void ClearChunks()
--- Assets/Scripts/LargeMarchingCube.cs
+++ Assets/Scripts/LargeMarchingCube.cs
@@ -1,30 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class LargeMarchingCube : MonoBehaviour
 {
-    [SerializeField] private Vector3Int globalDimensions = new Vector3Int(0, 0, 0);
-
     [SerializeField] private Vector3Int dimensions = new Vector3Int(0, 0, 0);
 
     [SerializeField][Range(0,1)] private float surfaceLevel = 0f;
-
-    //[SerializeField] private float noiseScale = 0f;
-    [SerializeField] private Vector3 noiseOffset = new Vector3(0, 0, 0);
-
     [SerializeField] private bool activateDebug = true;
     [SerializeField] private Color debugColor = Color.white;
 
     [SerializeField] private bool interpolate = true;
 
-    //[SerializeField] private int seed = 0;
-    //[SerializeField] private int octaves = 0;
-
-    private NoiseData noiseData;
-
     private float[,,] pointsNoise;
-    private float[,,] fallOffMapValues;
 
     private List<Vector3> vertices = new List<Vector3>();
     private List<int> triangles = new List<int>();
@@ -33,7 +24,7 @@ public class LargeMarchingCube : MonoBehaviour
 
     [SerializeField] private Vector3Int testCoords = new Vector3Int();
 
-    private CustomNoiseGen noiseGen;
+    private float[] noiseValues;
 
     private void Awake()
     {
@@ -93,19 +84,12 @@ public class LargeMarchingCube : MonoBehaviour
         }
     }
 
-    public void StartGeneration(float[,,] fallOffMapValues, CustomNoiseGen noiseGen, NoiseData noiseData, bool interpolate, Vector3Int globalDimensions,Vector3Int dimensions, float surfaceLevel, Vector3 noiseOffset)
+    public void StartGeneration(float[] noiseValues,  bool interpolate, Vector3Int dimensions, float surfaceLevel)
     {
-        this.fallOffMapValues = fallOffMapValues;
-
-        this.noiseGen = noiseGen;
-
-        this.noiseData = noiseData;
-
+        this.noiseValues = noiseValues;
         this.interpolate = interpolate;
-        this.globalDimensions = globalDimensions;
         this.dimensions = dimensions;
         this.surfaceLevel = surfaceLevel;
-        this.noiseOffset = noiseOffset;
 
         pointsNoise = new float[dimensions.x + 1, dimensions.y + 1, dimensions.z + 1];
 
@@ -179,65 +163,22 @@ public class LargeMarchingCube : MonoBehaviour
 
     private void GeneratePoints()
     {
-        for(int x=0; x< dimensions.x + 1; x++)
+        int i = 0;
+        for(int x=0; x< dimensions.x; x++)
         {
-            for(int y=0; y< dimensions.y + 1; y++)
+            for(int y=0; y< dimensions.y ; y++)
             {
-                for(int z=0; z< dimensions.z + 1; z++)
+                for(int z=0; z< dimensions.z; z++)
                 {
-                    int fallOffX = Mathf.RoundToInt(transform.localPosition.x) + x + globalDimensions.x / 2 - dimensions.x / 2;
-                    int fallOffY = Mathf.RoundToInt(transform.localPosition.y) + y + globalDimensions.y / 2 - dimensions.y / 2;
-                    int fallOffZ = Mathf.RoundToInt(transform.localPosition.z) + z + globalDimensions.z / 2 - dimensions.z / 2;
-                    pointsNoise[x, y, z] = Mathf.Clamp01(CalculateCustomNoise(x, y, z) - fallOffMapValues[fallOffX, fallOffY, fallOffZ]);
+                    pointsNoise[x, y, z] = noiseValues[i];
+                    i++;
                 }
             }
         }
     }
 
-    private float CalculateCustomNoise(float x, float y, float z)
-    {
-        float xNoise = (transform.position.x + x) / globalDimensions.x * noiseData.scale + noiseOffset.x;    // / globalDimensions.x * noiseScale
-        float yNoise = (transform.position.y + y) / globalDimensions.y * noiseData.scale + noiseOffset.y;    // / globalDimensions.x * noiseScale
-        float zNoise = (transform.position.z + z) / globalDimensions.z * noiseData.scale + noiseOffset.z;    // / globalDimensions.x * noiseScale
-
-        return noiseGen.OctavePerlin(xNoise, yNoise, zNoise, noiseData.octaves, noiseData.persistance, noiseData.seed);
-    }
-
-    private float CalculateNoise(float x, float y, float z)
-    {
-
-        float xNoise = (transform.position.x + x) / globalDimensions.x + noiseOffset.x;    // / globalDimensions.x * noiseScale
-        float yNoise = (transform.position.y + y) / globalDimensions.y + noiseOffset.y;    // / globalDimensions.x * noiseScale
-        float zNoise = (transform.position.z + z) / globalDimensions.z + noiseOffset.z;    // / globalDimensions.x * noiseScale
-
-        //return PerlinNoise3D(xNoise, yNoise, zNoise);
-        //return Mathf.PerlinNoise(xNoise, yNoise);
-        return LandMassNoise.Noise3D(xNoise, yNoise, zNoise, noiseData);
-    }
-
-    private float PerlinNoise3D(float x, float y, float z)
-    {
-        float AB = LandMassNoise.Noise( x, y, noiseData);
-        float BC = LandMassNoise.Noise(y, z, noiseData);
-        float AC = LandMassNoise.Noise(x, z, noiseData);
-
-        float BA = LandMassNoise.Noise(y, x, noiseData);
-        float CB = LandMassNoise.Noise(z, y, noiseData);
-        float CA = LandMassNoise.Noise(z, x, noiseData);
-
-        //Debug.Log($"<{x}> <{y}> <{z}>");
-        //Debug.Log($"<{AB}> <{BC}> <{AC}> <{BA}> <{CB}> <{CA}>");
-
-        return (AB + BC + AC + BA + CB + CA) / 6;
-        //return AB * BC * AC * BA * CB * CA;
-    }
-
-    /*private float _perlin3DFixed(float a, float b)
-    {
-        //return Mathf.PerlinNoise(a, b);
-        //return Mathf.Sin(Mathf.PI * Mathf.PerlinNoise(a, b));
-    }*/
-
+    
+    
     private int CalculateTriangulationIndex(float[] cubePoints)
     {
         int index = 0;
