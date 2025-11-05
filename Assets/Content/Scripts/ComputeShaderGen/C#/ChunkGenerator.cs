d2cb1898ea19655ac43c38b0c1781a4ca1f9ecab
SebastienMorris <85884910+SebastienMorris@users.noreply.github.com>
2025-11-05 02:30:48 +0100

    make more changes to ongui

--- Assets/Content/Scripts/ComputeShaderGen/C#/ChunkGenerator.cs
+++ Assets/Content/Scripts/ComputeShaderGen/C#/ChunkGenerator.cs
@@ -14,28 +14,27 @@ namespace Content.Scripts.ComputeShaderGen.C_
         [SerializeField] private bool Guizmo;
         [SerializeField] CameraController cameraController;
 
-        
+
         [SerializeField] private ComputeShader marchingCubesCompute;
         [SerializeField] private Shader renderShader;
-        
-        
-        [Header("Lighting")]
-        [SerializeField] private Light mainLight;
-        [SerializeField] private Vector3 mainLightRotation = Vector3.zero;
-        [SerializeField, Range(0.0f, 1.0f)] private float shadowStrength = 0.2f; 
-        
 
-        [Header("Texturing")]
-        [SerializeField] private IslandTexture textureData = IslandTexture.Default;
-        
-	
-        [Header("Generation")]
-        [SerializeField] private Vector3Int dimensions = new (0, 0, 0);
+
+        [Header("Lighting")] [SerializeField] private Light mainLight;
+        [SerializeField] private Vector3 mainLightRotation = Vector3.zero;
+        [SerializeField, Range(0.0f, 1.0f)] private float shadowStrength = 0.2f;
+
+
+        [Header("Texturing")] [SerializeField] private IslandTexture textureData = IslandTexture.Default;
+
+
+        [Header("Generation")] [SerializeField]
+        private Vector3Int dimensions = new(0, 0, 0);
+
         [SerializeField] [Range(0, 1)] private float surfaceLevel = 0.5f;
 
-        
-        [Header("Noise Properties")]
-        [SerializeField] private NoiseSettings noiseSettings = NoiseSettings.Default;
+
+        [Header("Noise Properties")] [SerializeField]
+        private NoiseSettings noiseSettings = NoiseSettings.Default;
 
         public Action<IslandTexture, float, Vector3Int, Vector3Int, NoiseSettings, float> OnValueUpdate;
 
@@ -44,13 +43,13 @@ namespace Content.Scripts.ComputeShaderGen.C_
         private bool initilised = false;
         private bool update = false;
         private bool isUpdating = false;
-        
+
         #region constants
-        
-            private int MAX_CHUNK_SIZE = 50;
+
+        private int MAX_CHUNK_SIZE = 50;
 
         #endregion
-        
+
         private void OnDrawGizmos()
         {
             if (Guizmo)
@@ -69,7 +68,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
         private void OnValidate()
         {
             if (!initilised || isUpdating) return;
-            
+
             OnValueUpdate?.Invoke(textureData, shadowStrength, dimensions, GetNumChunks(), noiseSettings, surfaceLevel);
             update = true;
         }
@@ -79,7 +78,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
             if (update)
             {
                 mainLight.transform.rotation = Quaternion.Euler(mainLightRotation);
-                
+
                 isUpdating = true;
                 CheckNewChunks();
                 update = false;
@@ -103,23 +102,23 @@ namespace Content.Scripts.ComputeShaderGen.C_
                 int sizeX = 0;
                 if (x >= numChunks.x - 1) sizeX = dimensions.x % MAX_CHUNK_SIZE;
                 sizeX = sizeX == 0 ? MAX_CHUNK_SIZE : sizeX;
-                
+
                 for (int y = 0; y < numChunks.y; y++)
                 {
                     int sizeY = MAX_CHUNK_SIZE;
                     if (y >= numChunks.y - 1) sizeY = dimensions.y % MAX_CHUNK_SIZE;
                     sizeY = sizeY == 0 ? MAX_CHUNK_SIZE : sizeY;
-                    
+
                     for (int z = 0; z < numChunks.z; z++)
-                    {  
+                    {
                         Vector3Int coord = new Vector3Int(x, y, z);
-                        
+
                         int sizeZ = MAX_CHUNK_SIZE;
                         if (z >= numChunks.z - 1) sizeZ = dimensions.z % MAX_CHUNK_SIZE;
                         sizeZ = sizeZ == 0 ? MAX_CHUNK_SIZE : sizeZ;
 
                         Vector3Int size = new Vector3Int(sizeX, sizeY, sizeZ);
-                        
+
                         AddChunk(coord, size);
 
                         numCurrentChunks.z++;
@@ -140,7 +139,8 @@ namespace Content.Scripts.ComputeShaderGen.C_
             obj.transform.parent = gameObject.transform;
 
             var chunk = obj.AddComponent<Chunk>();
-            chunk.Initialize(marchingCubesCompute, renderShader, textureData, shadowStrength, dimensions, coord, size, MAX_CHUNK_SIZE, noiseSettings, surfaceLevel);
+            chunk.Initialize(marchingCubesCompute, renderShader, textureData, shadowStrength, dimensions, coord, size,
+                MAX_CHUNK_SIZE, noiseSettings, surfaceLevel);
             chunk.OnChunkDestroyed += OnChunkDestroyed;
             OnValueUpdate += chunk.OnParamUpdate;
         }
@@ -155,37 +155,37 @@ namespace Content.Scripts.ComputeShaderGen.C_
                 int sizeX = 0;
                 if (x >= newNumChunks.x - 1) sizeX = dimensions.x % MAX_CHUNK_SIZE;
                 sizeX = sizeX == 0 ? MAX_CHUNK_SIZE : sizeX;
-                
+
                 for (int y = 0; y < newNumChunks.y; y++)
                 {
                     int sizeY = MAX_CHUNK_SIZE;
                     if (y >= newNumChunks.y - 1) sizeY = dimensions.y % MAX_CHUNK_SIZE;
                     sizeY = sizeY == 0 ? MAX_CHUNK_SIZE : sizeY;
-                    
+
                     for (int z = 0; z < newNumChunks.z; z++)
                     {
                         if (x < numCurrentChunks.x && y < numCurrentChunks.y && z < numCurrentChunks.z) continue;
-                        
+
                         Vector3Int coord = new Vector3Int(x, y, z);
-                        
+
                         int sizeZ = MAX_CHUNK_SIZE;
                         if (z >= newNumChunks.z - 1) sizeZ = dimensions.z % MAX_CHUNK_SIZE;
                         sizeZ = sizeZ == 0 ? MAX_CHUNK_SIZE : sizeZ;
-                        
+
                         Vector3Int size = new Vector3Int(sizeX, sizeY, sizeZ);
-                        
+
                         AddChunk(coord, size);
                     }
                 }
             }
-            
+
             numCurrentChunks = newNumChunks;
         }
 
         private void OnChunkDestroyed(Chunk chunk)
         {
             OnValueUpdate -= chunk.OnParamUpdate;
-            
+
             chunk.ConfirmDestroy();
         }
 
@@ -204,7 +204,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 600));
             GUILayout.BeginVertical("box");
-    
+
             GUILayout.Label("Generator Controls", GUI.skin.box);
             GUILayout.Label("Move     WASD");
             GUILayout.Label("Zoom     Mouse Scroll");
@@ -213,7 +213,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
             {
                 cameraController.Distance = newDist;
             }
-            
+
             GUILayout.Space(10);
             GUILayout.Label("Dimensions", EditorStyles.boldLabel);
             GUILayout.BeginHorizontal();
@@ -224,7 +224,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
             GUILayout.Label("Z:", GUILayout.Width(20));
             string newZ = GUILayout.TextField(dimensions.z.ToString(), GUILayout.Width(60));
             GUILayout.EndHorizontal();
-    
+
             if (int.TryParse(newX, out int x) && int.TryParse(newY, out int y) && int.TryParse(newZ, out int z))
             {
                 Vector3Int newDim = new Vector3Int(x, y, z);
@@ -234,7 +234,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
                     OnValidate();
                 }
             }
-            
+
             GUILayout.Space(10);
             GUILayout.Label($"Surface Level {surfaceLevel:F3}", EditorStyles.boldLabel);
             float newSurfaceLevel = GUILayout.HorizontalSlider(surfaceLevel, 0f, 1f);
@@ -243,54 +243,111 @@ namespace Content.Scripts.ComputeShaderGen.C_
                 surfaceLevel = newSurfaceLevel;
                 OnValidate();
             }
-            
+
             GUILayout.Space(5);
             GUILayout.Label($"Grass Ratio: {textureData.grassBlend:F3}", EditorStyles.boldLabel);
             float newGrassBlend = GUILayout.HorizontalSlider(textureData.grassBlend, 0f, 1f);
-            
+
             if (Mathf.Abs(newGrassBlend - textureData.grassBlend) > 0.001f)
             {
                 textureData.grassBlend = newGrassBlend;
                 OnValidate();
             }
-            
+
             GUILayout.Space(10);
-            
+
             GUILayout.Label("Seed", EditorStyles.boldLabel);
             GUILayout.BeginHorizontal();
             GUILayout.Label("X:", GUILayout.Width(20));
-            string newSeed = GUILayout.TextField(dimensions.x.ToString(), GUILayout.Width(60));
+            string stringSeed = GUILayout.TextField(noiseSettings.seed.ToString(), GUILayout.Width(60));
             GUILayout.EndHorizontal();
-            
-            GUILayout.Label("Noise",  EditorStyles.boldLabel);
+
+            int newSeed = 0;
+            if (int.TryParse(stringSeed, out int val)) newSeed = val;
+
+            GUILayout.Label("Noise", EditorStyles.boldLabel);
             GUILayout.Space(5);
             GUILayout.Label($"Frequency: {noiseSettings.frequency}", EditorStyles.boldLabel);
             int newFrequency = (int)GUILayout.HorizontalSlider(noiseSettings.frequency, 1.0f, 200.0f);
-            
+            if (noiseSettings.frequency != newFrequency)
+            {
+                noiseSettings.frequency = newFrequency;
+                OnValidate();
+            }
+
             GUILayout.Space(5);
             GUILayout.Label($"Octaves: {noiseSettings.octaves}", EditorStyles.boldLabel);
             int newOctaves = (int)GUILayout.HorizontalSlider(noiseSettings.octaves, 1.0f, 6.0f);
-            
+            if (noiseSettings.frequency != newFrequency)
+            {
+                noiseSettings.frequency = newFrequency;
+                OnValidate();
+            }
+
             GUILayout.Space(5);
             GUILayout.Label($"Lacunarity: {noiseSettings.lacunarity}", EditorStyles.boldLabel);
             int newLacunarity = (int)GUILayout.HorizontalSlider(noiseSettings.lacunarity, 2.0f, 4.0f);
-            
+            if (noiseSettings.frequency != newFrequency)
+            {
+                noiseSettings.frequency = newFrequency;
+                OnValidate();
+            }
+
             GUILayout.Space(5);
             GUILayout.Label($"Persistence: {noiseSettings.persistence}", EditorStyles.boldLabel);
             float newPersistence = GUILayout.HorizontalSlider(noiseSettings.persistence, 0.0f, 1.0f);
-            
+            if (Mathf.Abs(newPersistence - noiseSettings.persistence) > 0.001f)
+            {
+                noiseSettings.persistence = newFrequency;
+                OnValidate();
+            }
+
             GUILayout.Space(5);
             GUILayout.Label("ApplyFallOffMap", EditorStyles.boldLabel);
             GUILayout.BeginHorizontal();
             bool newApplyFallOff = GUILayout.Toggle(noiseSettings.applyFallOffMap, "");
             GUILayout.EndHorizontal();
-            
-            if (false)
+            if (newApplyFallOff != noiseSettings.applyFallOffMap)
             {
-                
+                noiseSettings.applyFallOffMap = newApplyFallOff;
                 OnValidate();
             }
-    
+
+            GUILayout.Space(5);
+            GUILayout.Label($"FallOff Steepness: {noiseSettings.steepness}", EditorStyles.boldLabel);
+            float newSteepness = GUILayout.HorizontalSlider(noiseSettings.steepness, 0.1f, 10.0f);
+            if (Mathf.Abs(newSteepness - noiseSettings.steepness) > 0.001f)
+            {
+                noiseSettings.steepness = newSteepness;
+                OnValidate();
+            }
+
+            GUILayout.Space(5);
+            GUILayout.Label($"FallOff Center Size: {noiseSettings.centerSize}", EditorStyles.boldLabel);
+            float newCenterSize = GUILayout.HorizontalSlider(noiseSettings.centerSize, 0.1f, 10.0f);
+            if (Mathf.Abs(newCenterSize - noiseSettings.centerSize) > 0.001f)
+            {
+                noiseSettings.centerSize = newCenterSize;
+                OnValidate();
+            }
+
+            if (noiseSettings.Compare(newSeed, newFrequency, newOctaves, newLacunarity, newPersistence, newApplyFallOff,
+                    newSteepness, newCenterSize))
+            {
+                noiseSettings.seed = newSeed;
+                noiseSettings.frequency = newFrequency;
+
+                noiseSettings.octaves = newOctaves;
+                noiseSettings.lacunarity = newLacunarity;
+                noiseSettings.persistence = newPersistence;
+
+                noiseSettings.applyFallOffMap = newApplyFallOff;
+                noiseSettings.steepness = newSteepness;
+                noiseSettings.centerSize = newCenterSize;
+
+                OnValidate();
+            }
+
             GUILayout.Space(10);
             GUILayout.Label("Lighting", EditorStyles.boldLabel);
             GUILayout.Space(5);
@@ -303,18 +360,9 @@ namespace Content.Scripts.ComputeShaderGen.C_
             GUILayout.Label("Z:", GUILayout.Width(20));
             string rotZ = GUILayout.TextField(mainLightRotation.z.ToString("F1"), GUILayout.Width(60));
             GUILayout.EndHorizontal();
-            
-            GUILayout.Space(5);
-            GUILayout.Label("Shadow Strength", EditorStyles.boldLabel);
-            float newShadow = GUILayout.HorizontalSlider(shadowStrength, 0f, 1f);
-            GUILayout.Label($"Value: {shadowStrength:F3}");
-            if (Mathf.Abs(newShadow - shadowStrength) > 0.001f)
-            {
-                shadowStrength = newShadow;
-                OnValidate();
-            }
-    
-            if (float.TryParse(rotX, out float rx) && float.TryParse(rotY, out float ry) && float.TryParse(rotZ, out float rz))
+
+            if (float.TryParse(rotX, out float rx) && float.TryParse(rotY, out float ry) &&
+                float.TryParse(rotZ, out float rz))
             {
                 Vector3 newRot = new Vector3(rx, ry, rz);
                 if (newRot != mainLightRotation)
@@ -324,15 +372,33 @@ namespace Content.Scripts.ComputeShaderGen.C_
                 }
             }
             
+            GUILayout.Space(5);
+            GUILayout.Label($"Intensity: {mainLight.intensity}", EditorStyles.boldLabel);
+            float newIntensity = GUILayout.HorizontalSlider(mainLight.intensity, 0f, 1f);
+            if (Mathf.Abs(newIntensity - mainLight.intensity) > 0.001f)
+            {
+                mainLight.intensity = newIntensity;
+                OnValidate();
+            }
+            
+            GUILayout.Space(5);
+            GUILayout.Label($"Shadow Strength {shadowStrength}", EditorStyles.boldLabel);
+            float newShadow = GUILayout.HorizontalSlider(shadowStrength, 0f, 1f);
+            if (Mathf.Abs(newShadow - shadowStrength) > 0.001f)
+            {
+                shadowStrength = newShadow;
+                OnValidate();
+            }
+
             GUILayout.Space(10);
             GUILayout.Label($"FPS: {(int)(1.0f / Time.unscaledDeltaTime)}");
-            
+
             GUILayout.Space(10);
             if (GUILayout.Button(Guizmo ? "Hide Gizmo" : "Show Gizmo"))
             {
                 Guizmo = !Guizmo;
             }
-    
+
             GUILayout.EndVertical();
             GUILayout.EndArea();
         }
@@ -345,7 +411,7 @@ namespace Content.Scripts.ComputeShaderGen.C_
         public Texture2D groundTexture;
 
         [Range(0.0f, 1.0f)] public float grassBlend;
-        
-        public static IslandTexture Default => new IslandTexture(){grassBlend = 0.5f};
+
+        public static IslandTexture Default => new IslandTexture() { grassBlend = 0.5f };
     }
 }
